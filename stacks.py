d07ac908f00118bc74ae6805d7f87f64e52d61c5
fahad-md-kamal <faahad.hossain@gmail.com>
2022-05-04 04:51:41 +0600

    - Reverse an string

---
+++ stacks.py
@@ -0,0 +1,34 @@
+class Stack:
+
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+    
+    def pop(self):
+        return self.items.pop()
+    
+    def is_empty(self):
+        return self.items == []
+    
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+    
+    def get_stack(self):
+        return self.items
+
+
+def reverse_string(stack, input_str):
+    for i in range(len(input_str)):
+        stack.push(input_str[i])
+    
+    rev_str = ""
+    while not stack.is_empty():
+        rev_str += stack.pop()
+    return rev_str
+
+stack = Stack()
+
+print(reverse_string(stack, 'Hellow'))
