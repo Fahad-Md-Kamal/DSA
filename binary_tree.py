d4ea428457d3bbccdc74ee11b0cb373fd003ab0c
fahad-md-kamal <faahad.hossain@gmail.com>
2022-05-16 08:27:40 +0600

    Implementation of Binay Tree

---
+++ binary_tree.py
@@ -0,0 +1,20 @@
+class Node:
+
+    def __init__(self, val) -> None:
+        self.val = val
+        self.left = None
+        self.right = None
+    
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
