a5885f18d13b751be6794f9dd51434131909038f
fahad-md-kamal <faahad.hossain@gmail.com>
2022-09-01 00:56:23 +0600

    TDD singley Linked list

--- linked-list/singley_linked_list.py
+++ linked-list/singley_linked_list.py
@@ -1,334 +1,243 @@
 import unittest
 
 
-class Node():
+# Create Node
+class Node:
+    def __init__(self, data, next_node=None) -> None:
+        self.data = data
+        self.next = next_node
 
-    def __init__(self, value, next=None):
-        self.value = value
-        self.next = next
-
-
-class LinkedList():
-    """
-    LinkedList
-    """
-    def __init__(self):
+# Create Linked List
+class LinkedList:
+    def __init__(self) -> None:
         self.head = None
+        self.tail = None
         self.count = 0
-    
-    def prepend_ll(self, val):
-        """
-        Insert Items to first of the linked list
 
-        Args:
-            val (numeric): pass numeric value. function will generate new node of it.
-        """
-        node = Node(val)
-        if self.head == None:
-            self.head = node
-        else:        
-            node.next = self.head
-            self.head = node
-        self.count +=1
-    
-    def display(self, statement=None):
-        """
-        DISPLAY Linked List
-
-        Args:
-            statement : Statement that you need to show after the linked list print. Defaults is set to ''/white space.
-        """
-        current_node = self.head
-        node_list = []
-        while current_node:
-            node_list.append(str(current_node.value))
-            current_node = current_node.next
-        if statement: node_list.append(f'{statement}')
-        return ' -> '.join(node_list)
-    
-    def __display_helper(self, node, _list=[]):
-        if node is None:
-            return
-        _list.append(str(node.value))
-        self.__display_helper(node.next, _list)
-        return(_list)
-    
-    def display_iter(self, statement=''):
-        """
-        DISPLAY Linked List RECURSIVE
-        """
-        res = self.__display_helper(self.head)
-        if statement:
-            res.append(statement)
-        return ' -> '.join(res)
-
-    
-    def append_ll(self, value):
-        """
-        Insert Node to the last of the linked list
-        Args:
-            value (numeric): Node to add
-        """
-        node = Node(value)
-        if self.head == None: return
-        current_node = self.head
-        while current_node.next:
-            current_node = current_node.next
-            self.count +=1
-        current_node.next = node
-    
-    def insert_at(self, index_point, value):
-        """
-        Insert Node at a certain Index number
-        Args:
-            index_point (Numeric): Nodex index
-            value (Node Value):
-        """
-        node = Node(value)
-        if not self.head: return
-        if index_point == 0:
-            node.next = self.head
-            self.head = node
-            return
-        idx = 0
+    # Display Iterative
+    def display(self):
+        if not self.head: return []
+        data_list = []
         current_node = self.head
         while current_node:
-            if index_point - 1 == idx:
-                node.next = current_node.next
-                current_node.next = node
-                self.count +=1
+            data_list.append(str(current_node.data))
             current_node = current_node.next
-            idx += 1
+        return ' -> '.join(data_list)
     
-    def insert_after(self, old_node, new_val):
-        """
-        Insert Node After
-        Args:
-            old_node (Node Value):
-            new_val (Node Value):
-        """
-        new_node = Node(new_val)
-        if not self.head: return
-
-        current_node = self.head
-        while current_node:
-            if current_node.value == old_node:
-                new_node.next = current_node.next
-                current_node.next = new_node
-                self.count += 1
-            current_node = current_node.next
-    
-    def next_node(self, node_value) -> Node:
-        """
-        Given Node's Next Node
-        Args:
-            node_value (Node Value):
-
-        Returns:
-            Node: 
-        """
-        current_node = self.head
-        while current_node.next:
-            if current_node.next.value == node_value:
-                return current_node.next.next
-            current_node = current_node.next
-        return None
-
-    def previous_node(self, node_value) -> Node:
-        """
-        Get Previous node of a given node value
-        Args:
-            node_value (Node Value):s
-        Returns:
-            Node:s
-        """
-        current_node = self.head
-        while current_node.next:
-            if current_node.next.value == node_value:
-                return current_node
-            current_node = current_node.next
-        return None
-
-    def insert_before(self, target_node, new_val):
-        """
-        Insert Before a node
-        Args:
-            target_node (_type_):s
-            new_val (_type_):s
-        """
-        if not self.head: return
-        new_node = Node(new_val)
-        prev_node = self.previous_node(target_node)
-        if prev_node:
-            new_node.next = prev_node.next
-            prev_node.next = new_node
-            self.count +=1
-
-    def delete_by_val(self, node_to_delete):
-        """
-        Delete Node by value
-        Args:
-            node_to_delete (_type_):s
-        """
-        if not self.head: return
-        if self.head.value == node_to_delete:
-            self.head = self.head.next
-            self.count -= 1
-            return
-        prev_node = self.previous_node(node_to_delete)
-        next_node = self.next_node(node_to_delete)
-        if prev_node:
-            prev_node.next = next_node
-            self.count -= 1
-
-    def delete_by_index(self, node_index):
-        """
-        DELETE Node by Index
-
-        Args:
-            node_index (_type_):
-        """
-        if not self.head: return
-        if node_index == 0:
-            self.head = self.head.next
-            self.count -= 1
-            return
-        idx = 0
-        current_node = self.head
-        while current_node.next:
-            if idx+1 == node_index:
-                current_node.next = current_node.next.next
-                self.count -= 1
-            idx += 1
-            current_node = current_node.next
-
-    def search_element_by_value(self, node_value):
-        """
-        Search Node by Index
-
-        Args:
-            node_index (_type_):
-        """
-        if not self.head: return
-
-        if node_value == self.head.value:
-            return 0
-
-        idx = 0
-        current_node = self.head
-        while current_node.next:
-            if current_node.value == node_value:
-                return idx
-            idx += 1
-            current_node = current_node.next
-
-        if current_node.value == node_value:
-            return idx
+    # Helper for recursive Process
+    def _display_helper(self, node, data_list=[]) -> list:
+        if not node: return
+        data_list.append(str(node.data))
+        self._display_helper(node.next, data_list)
+        return data_list
+            
+    # Display Reverse
+    def display_recur(self):
+        if not self.head: return []
+        data_list = self._display_helper(self.head)
+        return ' -> '.join(data_list)
         
-        return -1
-
-    def search_element_index(self, node_index):
-        """
-        Search Node by Index
-
-        Args:
-            node_index (_type_):
-        """
-        if not self.head: return
-        if node_index == 0:
-            return self.head.value
-        idx = 0
+    # Append / Insert at End
+    def append(self, data):
+        node = Node(data)
+        if not self.head:
+            self.head = node
+            return
         current_node = self.head
         while current_node.next:
-            if idx == node_index:
-                return current_node.value
-            idx += 1
             current_node = current_node.next
-        return -1
+        current_node.next = node
+        self.tail = node
+        self.count += 1
 
-    def delete_list(self):
-        self.head = None
+    # Prepend / Insert at Begining
+    def prepend(self, data):
+        node = Node(data)
+        if not self.head:
+            self.head = node
+            self.tail = node
+            self.count += 1
+            return
+        node.next = self.head
+        self.head = node
+        self.count += 1
     
-    def list_length(self):
-        if not self.head: return 0
+    def append_list_from_array(self, data_list):
+        if not isinstance(data_list, list): return
+        for data in data_list:
+            self.append(data)
+        
+    # Previous Node by index
+    def previous_index_node(self, index):
+        if not self.head: return
         count = 0
         current = self.head
         while current:
-            count +=1
+            if count == index - 1:
+                return current
             current = current.next
-        return count
+            count += 1
+        return None
+
+    # After Node by index
+    def next_index_node(self, index):
+        if not self.head: return
+        count = 0
+        current_node = self.head
+        while current_node and current_node.next:
+            if count == index:
+                return current_node.next
+            current_node = current_node.next
+            count +=1
+        return None
+
+    # Previous Node by val
+    def previous_node_of_val(self, data):
+        if not self.head: return
+        current_node = self.head
+        while current_node and current_node.next:
+            if current_node.next.data == data:
+                return current_node.data
+            current_node = current_node.next
+        return None
+
+    # Next Node by val
+    def next_node_of_val(self, data):
+        if not self.head: return
+        current_node = self.head
+        while current_node and current_node.next:
+            if current_node.data == data:
+                return current_node.next.data
+            current_node = current_node.next
+        return
+
+    # Insert at index
+    def insert_at_index(self, index, data):
+        if not self.head: return
+        new_node = Node(data)
+
+        prev_node = self.previous_index_node(index)
+        if prev_node: 
+            new_node.next = prev_node.next
+            prev_node.next = new_node
+
+    # Insert before index
+    # Insert After index
+
+    # Insert before value
+    # Insert After value
+
+    # Remove at index
+    # Remove before index
+    # Remove after index
+
+    # Remove before value
+    # Remove after value
 
 class TestSingleyLL(unittest.TestCase):
 
     def setUp(self) -> None:
         self.ll = LinkedList()
-        self.ll.prepend_ll(3)
-        self.ll.prepend_ll(4)
         return super().setUp()
     
-    def test_display_func(self):
-        self.assertEqual(self.ll.display(), '4 -> 3')
+    def test_display_with_empty(self):
+        self.assertEqual(self.ll.display(), [])
+    
+    def test_display_rec_with_empty(self):
+        self.assertEqual(self.ll.display_recur(), [])
+    
+    def test_append(self):
+        self.ll.append(9)
+        self.ll.append(12)
+        self.assertEqual(self.ll.display(), '9 -> 12')
+    
+    def test_prepend(self):
+        self.ll.prepend(5)
+        self.ll.prepend(2)
+        self.assertEqual(self.ll.display(), '2 -> 5')
+    
+    def test_prepend_with_single_value(self):
+        self.ll.prepend(33)
+        self.assertEqual(self.ll.display(), '33')
+    
+    def test_append_list_from_data_list(self):
+        self.ll.append_list_from_array([1,2,3,4])
+        self.assertEqual(self.ll.display(), '1 -> 2 -> 3 -> 4')
+    
+    def test_create_list_from_data_empty_list(self):
+        self.ll.append_list_from_array([])
+        self.assertEqual(self.ll.display(), [])
+    
+    def test_previous_index_node(self):
+        self.ll.append_list_from_array([9,12,15])
+        node = self.ll.previous_index_node(2)
+        self.assertEqual(node.data, 12)
+    
+    def test_previous_index_node_for_first(self):
+        self.ll.append_list_from_array([9, 12])
+        node = self.ll.previous_index_node(1)
+        self.assertEqual(node.data, 9)
+    
+    def test_previous_index_node_for_out_of_bound(self):
+        self.ll.append_list_from_array([9, 17])
+        self.assertEqual(self.ll.previous_index_node(10), None)
+    
+    def test_previous_index_node_for_zero_index(self):
+        self.ll.append(9)
+        self.assertEqual(self.ll.previous_index_node(0), None)
+    
+    def test_next_index_node(self):
+        self.ll.append_list_from_array([9, 12, 15])
+        node = self.ll.next_index_node(0)
+        self.assertEqual(node.data, 12)
+    
+    def test_next_index_node_negative_index(self):
+        self.ll.append_list_from_array([9, 12])
+        node = self.ll.next_index_node(-1)
+        self.assertEqual(node, None)
+    
+    def test_next_index_node_last_index(self):
+        self.ll.append_list_from_array([9, 12])
+        node = self.ll.next_index_node(-1)
+        self.assertEqual(node, None)
+    
+    def test_previous_node_of_data(self):
+        self.ll.append_list_from_array([9, 12])
+        self.assertEqual(self.ll.previous_node_of_val(12), 9)
 
-    def test_display_iter_func(self):
-        self.assertEqual(self.ll.display_iter(), '4 -> 3')
+    def test_previous_node_of_data_empty(self):
+        self.assertEqual(self.ll.previous_node_of_val(1), None)
 
-    def test_list_insert_first(self):
-        self.ll.prepend_ll(6)
-        self.assertEqual(self.ll.display(), '6 -> 4 -> 3')
+    def test_previous_node_of_data_out_bound(self):
+        self.ll.append(13)
+        self.assertEqual(self.ll.previous_node_of_val(2), None)
+    
+    def test_next_node_of_val(self):
+        self.ll.append_list_from_array([33, 66])
+        self.assertEqual(self.ll.next_node_of_val(33), 66)
+    
+    def test_next_node_of_val_not_in_list(self):
+        self.ll.append_list_from_array([33, 66])
+        self.assertEqual(self.ll.next_node_of_val(44), None)
+    
+    def test_next_node_of_val_emapty_list(self):
+        self.assertEqual(self.ll.next_node_of_val(44), None)
+    
+    def test_insert_at_index_empty_list(self):
+        self.ll.append_list_from_array([])
+        self.ll.insert_at_index(index=1, data=44)
+        self.assertEqual(self.ll.display(), [])
+    
+    def test_insert_at_index(self):
+        self.ll.append_list_from_array([1,2,3,4])
+        self.ll.insert_at_index(index=1, data=44)
+        self.assertEqual(self.ll.display(), '1 -> 44 -> 2 -> 3 -> 4')
+    
+    def test_insert_at_index_last_index(self):
+        self.ll.append_list_from_array([1,2,3,4])
+        self.ll.insert_at_index(index=5, data=44)
+        self.assertEqual(self.ll.display(), '1 -> 2 -> 3 -> 4')
+    
 
-    def test_list_insert_last(self):
-        self.ll.append_ll(55)
-        self.ll.append_ll(66)
-        self.assertEqual(self.ll.display(), '4 -> 3 -> 55 -> 66')
-
-    def test_list_insert_after(self):
-        self.ll.insert_after(3, 27)
-        self.assertEqual(self.ll.display(), '4 -> 3 -> 27')
-
-    def test_list_insert_at(self):
-        self.ll.insert_at(1, 27)
-        print(self.ll.display())
-        self.assertEqual(self.ll.display(), '4 -> 27 -> 3')
-
-        
-if __name__=="__main__":
+if __name__ == '__main__':
     unittest.main()
-
-    # ll.display('INSERT FIRST')
-
-    # ll.insert_last(44)
-    # ll.insert_first(88)
-    # # ll.display('INSERT LAST 44')
-
-    # ll.insert_at(2, 99)
-    # # ll.display('INSERT AT 2nd pos')
-
-    # ll.insert_after(old_node=99,new_val=77)
-    # # ll.display('INSERT AFTER 99')
-
-    # ll.insert_before(4, 2)
-    # # ll.display('INSERT BEFORE 4')
-
-    # ll.delete_by_val(88)
-    # # ll.display('DELETE BY VAL 88')
-
-    # ll.delete_by_val(44)
-    # # ll.display('DELETE BY VAL 44')
-
-    # ll.delete_by_index(0)
-    # # ll.display('DELETE BY INDEX 0')
-
-    # # ll.display()
-    # ll.display_iter("RECURSIVE")
-
-    # node = ll.search_element_by_value(77)
-    # print(node)
-
-    # node = ll.search_element_index(2)
-    # print(node)
-
-    # lenght = ll.list_length()
-    # # ll.display(f'List Lenght {lenght}')
-
-    # # node = ll.delete_list()
-    # # ll.display()
