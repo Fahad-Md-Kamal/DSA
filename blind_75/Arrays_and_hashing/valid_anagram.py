c46492e0bb90011c11674dcf239391dea3bf69b8
fahad.kamal <fahad.kamal@bjitgroup.com>
2026-05-19 13:05:14 +0600

    Solution for anagram

--- blind_75/Arrays_and_hashing/valid_anagram.py
+++ blind_75/Arrays_and_hashing/valid_anagram.py
@@ -34,16 +34,16 @@ class Solution:
         if len(s) != len(t):
             return False
         
-        tmp_dict = {}
-        for c in s:
-            tmp_dict[c] = tmp_dict.get(c, 0) + 1
-            
-        for c in t:
-            c_v = tmp_dict.get(c, 0)
-            if c_v <= 0:
+        tmp_dict = dict()
+        for ch in s:
+            tmp_dict[ch] = tmp_dict.get(ch, 0) + 1
+        
+        for ch in t:
+            val = tmp_dict.get(ch, 0)
+            if val <= 0:
                 return False
-            tmp_dict[c] = c_v - 1
-        return True
+            tmp_dict[ch] = val - 1
+        return sum(tmp_dict.values()) == 0
             
 
 if __name__ == "__main__":
